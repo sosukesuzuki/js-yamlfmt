fd1bd73a79cedebbce99350e68dca524e5bcf344
Sosuke Suzuki <aosukeke@gmail.com>
2022-08-27 17:44:08 +0900

    Implement main.go

--- packages/wasm/go.mod
+++ packages/wasm/go.mod
@@ -1,3 +1,12 @@
 module github.com/sosukesuzuki/js-yamlfmt
 
 go 1.19
+
+require github.com/google/yamlfmt v0.2.0
+
+require (
+	github.com/bmatcuk/doublestar/v4 v4.2.0 // indirect
+	github.com/google/go-cmp v0.5.8 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- packages/wasm/main.go
+++ packages/wasm/main.go
@@ -1,4 +1,41 @@
 package main
 
+import (
+	"syscall/js"
+
+	"github.com/google/yamlfmt"
+	"github.com/google/yamlfmt/formatters/basic"
+)
+
+var global = js.Global().Get("global")
+
 func main() {
+	js.Global().Set("runYamlFmt", js.FuncOf(runYamlFmt))
+	select {}
+}
+
+func fail(err error, when string) {
+	global.Call("throwError", err.Error()+" on "+when)
+}
+
+func runYamlFmt(this js.Value, args []js.Value) interface{} {
+	source := args[0].String()
+	result, err := run(getFullRegistry(), []byte(source))
+	if err != nil {
+		fail(err, "format failed")
+	}
+	return string(result)
+}
+
+func run(registry *yamlfmt.Registry, source []byte) ([]byte, error) {
+	factory, err := registry.GetDefaultFactory()
+	if err != nil {
+		return nil, err
+	}
+	formatter := factory.NewDefault()
+	return formatter.Format(source)
+}
+
+func getFullRegistry() *yamlfmt.Registry {
+	return yamlfmt.NewFormatterRegistry(&basic.BasicFormatterFactory{})
 }
