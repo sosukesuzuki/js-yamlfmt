36bbb00ec2e73f85f2341a98798fa8f66d6374cc
Sosuke Suzuki <aosukeke@gmail.com>
2022-08-27 23:14:28 +0900

    Complex

--- packages/node/src/index.ts
+++ packages/node/src/index.ts
@@ -1,10 +1,27 @@
 import { runYamlFmt } from "@js-yamlfmt/wasm";
 
 const foo = `
-type: "library"
-foo:
-- bar
-- baz
+type: "tool"
+language: "typescript"
+project:
+  name: "node"
+  description: "yamlfmt CLI working on Node.js"
+  owner: "sosukesuzuki"
+  maintainers: ["sosukesuzuki"]
+  channel: "#na"
+
+dependsOn:
+  - "wasm"
+
+workspace:
+  inheritedTasks:
+    rename:
+      buildPackage: "build"
+
+tasks:
+  typecheck:
+    args:
+      - "--force"
 `;
 
 runYamlFmt(foo).then((f) => console.log(f));
