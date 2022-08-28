1b248ed362ce60d6a05a14dbdf6b35e56524fa4d
Sosuke Suzuki <aosukeke@gmail.com>
2022-08-28 16:41:35 +0900

    Fix

--- packages/wasm/scripts/cp-wasm-exec.sh
+++ packages/wasm/scripts/cp-wasm-exec.sh
@@ -1,6 +1,9 @@
 GOROOT=`go env GOROOT`
 
 PROJECT_DIR=$(cd $(dirname $(dirname $0)); pwd)
-echo $PROJECT_DIR
+pwd
+ls $(pwd)
+ls .
+ls $PROJECT_DIR
 
-cp $GOROOT/misc/wasm/wasm_exec.js $PROJECT_DIR/src/wasm_exec.js
+cp $GOROOT/misc/wasm/wasm_exec.js ./src/wasm_exec.js
